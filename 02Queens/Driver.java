9cc0610ba1cd99f175ea9a669340a0cd9b277ccc
Katherine Pan <kpan2@stuy.edu>
2016-02-10 13:11:28 -0500

    worked in class

---
+++ 02Queens/Driver.java
@@ -0,0 +1,4 @@
+public class Driver{
+    public static void main(String[]args){
+	Queens g = new Queens(12);
+	
--- 02Queens/Queens.java
+++ 02Queens/Queens.java
@@ -1,9 +1,9 @@
 public class Queens{
-    int size;
-    int[][] grid;
+   private  int size;
+   private  int[][] grid;
 
     public Queens(int length){
-	size = length;
+	size=length;
 	grid = new  int [length][length];
 	for(int i=0;i<length;i++){
 	    for(int j=0;j<length;j++){
@@ -11,17 +11,62 @@ public class Queens{
 	    }
 	}
     }
-    public boolean addQueens(int row, int col){
-	if(grid[row][col]!=0){
-	    return false;}
-	else{
-	    grid[row][col]+=1;}
-	    
-    }
-	public boolean removeQueens(int row, int col){
-    }
-    
-	public void change(int row,int col,int increment){
-	    grid[row][col]+=increment;
+
+
+    public void printSolution(){
+	String ans = "";
+	for(int i=0;i<size;i++){
+	    for(int j=0;j<size;j++){
+		if(grid[i][j]==-1){
+		    ans+="Q"+"\t";
+		}
+		else{
+		    ans+="_"+"\t";
+		}
+	    }
+	    ans+="\n";
 	}
+	System.out.println(ans);
+    }
+    public boolean addQueens(int row, int col){
+        if(grid[row][col]!=0){
+	    return false;}
+	int increment = 1;
+	grid[row][col]=1;
+	while(col+increment<size){
+	    grid[row][col+increment]--;
+	    if(row+increment<size){
+		grid[row+increment][col+increment]--;
+	    }
+	    if(row-increment>=0){
+		grid[row-increment][col+increment]--;
+	    }
+	    increment ++;
+	}
+	return true;
+    }
+	    
+
+	public boolean removeQueens(int row, int col){
+	    if(grid[row][col]!=1){
+		return false;}
+	    int increment = 1;
+	    grid[row][col]=1;
+	    while(col+increment<size){
+		grid[row][col+increment]++;
+		if(row+increment<size){
+		    grid[row+increment][col+increment]++;
+		}
+		if(row-increment>=0){
+		    grid[row-increment][col+increment]++;
+		}
+		increment ++;
+	}
+	return true;
+    }
+
+    public String toString(){
+	
+    }
+
 }
