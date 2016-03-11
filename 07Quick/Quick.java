e2f65d756196b71e62d79663c47a91d185fa508d
Katherine Pan <kpan2@stuy.edu>
2016-03-11 13:10:33 -0500

    worked on improving partition

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,40 +1,51 @@
 import java.util.*;
 public class Quick{
-    private static int partition(int[] data,int left,int right){
+
+    private static int[] partition(int[] data,int left,int right){
+	int rnd = (int)(Math.random()*(right-left)+left);
+	System.out.println(data[rnd]);
+	int[] newAry = new int[data.length];
+	int temp=data[rnd];
+	int start = left;
+	int end = right;
+	for(int i=left;i<right;i++){
+	    if(data[i]<temp){
+		newAry[start]=data[i];
+		start++;}
+	    if(data[i]>temp){
+		newAry[end]=data[i];
+		end--;}
+	}
+	int[] ans={start,end}; 
+	for(int j=start;j<end;j++){
+	    newAry[start]=temp;
+	}
+	
+	data=newAry;
+	System.out.println(Arrays.toString(data));
+	return ans;
+    }
+    private static int partitionOld(int[] data,int left,int right){
 	int rnd = (int)(Math.random()*(right-left)+left);
 	int temp=data[rnd];
 	int end = right;
-	//	System.out.println("random num is: "+ data[rnd]);
-	//	System.out.println(Arrays.toString(data));
 	swap(data,rnd,right);
-	//	System.out.println(Arrays.toString(data));
 	right--;
-	//	System.out.println("initialLeft: "+ left+" initialRight: "+right);
 	while(right>left){
 	    if(data[left]>temp){
 		swap(data,left,right);
-		//	System.out.println("swaped "+left+" and "+right);
 		right--;
-		//	System.out.println("changed right: "+right);
-		//	System.out.println(Arrays.toString(data));
 	    }
 	    if(data[left]<=temp){
 		left++;
-		//	System.out.println("changed left: "+left);
-		//	System.out.println(Arrays.toString(data));
 	    }
 	    if(data[right]<temp){
 		swap(data,right,left);
-		//	System.out.println("swaped "+left+" and "+right);
 		left++;
-		//	System.out.println("changed left: "+left);
-		//	System.out.println(Arrays.toString(data));
 	    }
 	    }
 	    if(data[right]>=temp){
 		right--;
-		//	System.out.println("changed right: "+right);
-		//	System.out.println(Arrays.toString(data));
 	    }
   
     
@@ -55,7 +66,7 @@ public class Quick{
     
     public static void quickSort(int[] data,int left,int right){
 	if(right - left >0){
-	    int num = partition(data,left,right);
+	    int num = partitionOld(data,left,right);
 	    quickSort(data,left,num-1);
 	    quickSort(data,num+1,right);
 	}
@@ -67,7 +78,7 @@ public class Quick{
 
     public static int quickselect(int[] data,int k,int start,int end){
 	if(start<end){
-	    int num = partition(data,start,end);
+	    int num = partitionOld(data,start,end);
 	    if(num==k){
 		return data[num];}
 	    if(num>k){
@@ -91,11 +102,11 @@ public class Quick{
 
     public static void main(String[]args){
 	Quick q = new Quick();
-	int[] data = {2,10,5,-22,48,4,7,8,34,-1,89,-30};
-	System.out.println(q.partition(data,0,11));
-		System.out.println(q.quickselect(data,4));
-	 	q.quickSort(data);
-	       	System.out.println(Arrays.toString(data));
+	int[] data = {2,10,5,2,4,4,10,10,4,-1,5,2};
+	System.out.println(Arrays.toString(q.partition(data,0,11)));
+	//	System.out.println(q.quickselect(data,4));
+	//	q.quickSort(data);
+	// 	System.out.println(Arrays.toString(data));
 
     }
 }
