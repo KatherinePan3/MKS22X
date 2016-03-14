be38300742aac91fbc59e2230201990e029241d1
Katherine <kpan2@stuy.edu>
2016-03-14 00:33:58 -0400

    finished new partition and quicksort, but they're slow

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -3,12 +3,13 @@ public class Quick{
 
     private static int[] partition(int[] data,int left,int right){
 	int rnd = (int)(Math.random()*(right-left)+left);
-	System.out.println(data[rnd]);
+	//	System.out.println(data[rnd]);
+	//	System.out.println(Arrays.toString(data)+" before change");
 	int[] newAry = new int[data.length];
 	int temp=data[rnd];
 	int start = left;
 	int end = right;
-	for(int i=left;i<right;i++){
+	for(int i=left;i<=right;i++){
 	    if(data[i]<temp){
 		newAry[start]=data[i];
 		start++;}
@@ -17,12 +18,13 @@ public class Quick{
 		end--;}
 	}
 	int[] ans={start,end}; 
-	for(int j=start;j<end;j++){
-	    newAry[start]=temp;
-	}
+	for(int j=start;j<=end;j++){
+	  newAry[j]=temp;
+		}
 	
-	data=newAry;
-	System.out.println(Arrays.toString(data));
+	for(int a=left;a<=right;a++){
+		data[a]=newAry[a];
+	    }
 	return ans;
     }
     private static int partitionOld(int[] data,int left,int right){
@@ -59,32 +61,45 @@ public class Quick{
 
 	
     }
-    
-    public static void quickSort(int[] data){
+  public static void quickSort(int[] data){
 	quickSort(data,0,data.length-1);
     }
     
     public static void quickSort(int[] data,int left,int right){
-	if(right - left >0){
-	    int num = partitionOld(data,left,right);
-	    quickSort(data,left,num-1);
-	    quickSort(data,num+1,right);
+	if(left<right){
+	    int[] num = partition(data,left,right);
+	    quickSort(data,left,num[0]-1);
+	    quickSort(data,num[1]+1,right);
 	}
     }
 
-    public static int quickselect(int[] data,int k){
-	return quickselect(data,k,0,data.length-1);
+
+    
+    public static void quickSortOld(int[] data){
+	quickSortOld(data,0,data.length-1);
+    }
+    
+    public static void quickSortOld(int[] data,int left,int right){
+	if(right - left >0){
+	    int num = partitionOld(data,left,right);
+	    quickSortOld(data,left,num-1);
+	    quickSortOld(data,num+1,right);
+	}
     }
 
-    public static int quickselect(int[] data,int k,int start,int end){
+    public static int quickSelectOld(int[] data,int k){
+	return quickSelectOld(data,k,0,data.length-1);
+    }
+
+    public static int quickSelectOld(int[] data,int k,int start,int end){
 	if(start<end){
 	    int num = partitionOld(data,start,end);
 	    if(num==k){
 		return data[num];}
 	    if(num>k){
-		return quickselect(data,k,start,num);}
+		return quickSelectOld(data,k,start,num);}
 	    if(num<k){
-		return quickselect(data,k,num+1,end);}
+		return quickSelectOld(data,k,num+1,end);}
 	}
 	return data[k];
     }
@@ -102,11 +117,25 @@ public class Quick{
 
     public static void main(String[]args){
 	Quick q = new Quick();
-	int[] data = {2,10,5,2,4,4,10,10,4,-1,5,2};
-	System.out.println(Arrays.toString(q.partition(data,0,11)));
-	//	System.out.println(q.quickselect(data,4));
-	//	q.quickSort(data);
-	// 	System.out.println(Arrays.toString(data));
-
+	int[] d = new int[40000];
+	int[] c = new int[d.length];
+	int[] e = new int[d.length];
+	for(int i=0;i<d.length;i++){
+	    d[i]=(int)(Math.random()*3);
+	    c[i]=d[i];
+	    e[i]=d[i];
+	}
+	long startTime=System.currentTimeMillis();
+	q.quickSort(d);
+	long endTime=System.currentTimeMillis();
+	System.out.println(endTime-startTime);
+	long startTime2=System.currentTimeMillis();
+	Arrays.sort(c);
+	long endTime2=System.currentTimeMillis();
+	System.out.println(endTime2-startTime2);
+	long startTime3=System.currentTimeMillis();
+	q.quickSortOld(e);;
+	long endTime3=System.currentTimeMillis();
+	System.out.println(endTime3-startTime3);
     }
 }
