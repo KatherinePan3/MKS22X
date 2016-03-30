5b248c202dedf317e1ec4628d9d2906552d5c2bc
Katherine Pan <kpan2@stuy.edu>
2016-03-30 13:10:06 -0400

    started driver for myStack

--- 09StackQueue/MyStack.java
+++ 09StackQueue/MyStack.java
@@ -46,5 +46,40 @@ public class MyStack<T>{
     public boolean isEmpty(){
 	return size()==0;
     }
+
+    public String toString(){
+	return data.toString();
+    }
+
+    public static void main(String[]args){
+	MyStack<Integer> data = new MyStack<Integer>();
+	MyStack<Integer> data2 = new MyStack<Integer>();
+	
+	System.out.println("testing peek and pop on empty stack: ");
+	System.out.println(data.peek());
+	data.pop();
+	System.out.println(data);
+       
+	
+	
+	for(int i=0;i<10;i++){
+	    data.add(i);
+	    data2.add(i);
+	}
+	System.out.println("added numbers 1-9 to both stacks");
+	System.out.println(data+"size: "+data.size());
+	System.out.println(data2+"size: "+data2.size());
+	
+	System.out.println("remove: ");
+	data.remove();
+	data2.remove();
+	System.out.println(data);
+	System.out.println(data2);
+
+
+	
+
+	
+    }
 }
 
