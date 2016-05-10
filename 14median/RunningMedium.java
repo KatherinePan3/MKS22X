9524deb1f782d25d34366b9660bac9cccc29d4ae
Katherine Pan <kpan2@stuy.edu>
2016-05-10 13:12:06 -0400

    started running medium

---
+++ 14median/RunningMedium.java
@@ -0,0 +1,19 @@
+public class RunningMedian{
+    private MyHeap maxH;
+    private MyHeap minH;
+    public RunningMedian(){
+    }
+
+    public double getMedian(){		
+	double median=0.0;
+	if(med.getSize()==0){
+	    throw new NoSuchElementException();}
+	return median;
+    }
+
+    public void add(Integer x){
+	
+    }
+
+
+}
